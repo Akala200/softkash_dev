5ecc5f74b1e7b10ac49aff5a00987fe70d41a9d8
Adeniyi <adeiyiakala91@gmail.com>
2020-10-22 15:17:56 +0100

    missing dependecy included

--- models/User.js
+++ models/User.js
@@ -35,10 +35,14 @@ const userSchema = new Schema({
         unique: true,
         validate: {
             validator: function(v) {
-                return /d{11}/.test(v);
+                return /d{10}/.test(v);
             },
             message: '{VALUE} is not a valid 10 digit number!'
         }
+    },
+    contry_code: {
+        type: String,
+        required: true
     },
     password: {
         type: String,
