b05d4f9be84a9231beb18577d327905f1f914ae9
Adeniyi <adeiyiakala91@gmail.com>
2020-10-22 15:07:16 +0100

    missing dependecy included

--- models/User.js
+++ models/User.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const {Schema} = mongoose;
 
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const userSchema = new Schema({
     first_name: {
         type: String,
@@ -17,13 +23,22 @@ const userSchema = new Schema({
     },      
     email: {
         type: String,
-        required: false,
-        unique: true
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     phone: {
-        type: String,
-        required: true,
-        unique: true
+        type: Number,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /d{11}/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
     },
     password: {
         type: String,
